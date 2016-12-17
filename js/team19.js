43d46b25c3691959c5795263b7a340e91a7de6a9
etapaza <etapaza@umich.edu>
2016-12-17 17:15:30 -0500

    some mods

--- js/team19.js
+++ js/team19.js
@@ -1,21 +1,4 @@
-// $(document).ready(function(){
 
-// 	getCurrentTabUrl(showURL);
-
-// });
-
-// var showURL=function(url){
-// 	var parser = document.createElement('a');
-// 	parser.href = url;
-// 	var domain=parser.hostname.replace(/^www\./,'');
-// 	getCookies(domain);
-// };
-
-// function getCookies(filter){
-// 	chrome.cookies.getAll({'domain':filter},function(cookies) {
-// 		console.log(cookies);
-// 	});
-// }
 
 chrome.tabs.query({
     currentWindow: true,
@@ -24,13 +7,13 @@ chrome.tabs.query({
     chrome.webNavigation.getAllFrames({
         tabId: tabs[0].id
     }, function(details) {
-        // Get unique list of URLs
+        
         var urls = details.reduce(function(urls, frame) {
             if (urls.indexOf(frame.url) === -1)
                 urls.push(frame.url);
             return urls;
         }, []);
-        // Get all cookies
+        
         var index = 0;
         var cookies = [];
         urls.forEach(function(url) {
@@ -39,12 +22,10 @@ chrome.tabs.query({
             }, function(additionalCookies) {
                 cookies = cookies.concat(additionalCookies);
                 if (++index === urls.length) {
-                    // Collected all cookies!
-                    // TODO: Use cookies.
-                    // Note: The array may contain duplicate cookies!
+                    
                     console.log(cookies);
                 }
-            }); // chrome.cookies.getAll
-        }); // urls.forEach
-    }); // chrome.webNavigation.getAllFrames
-}); // chrome.tabs.query
+            }); 
+        }); 
+    }); 
+}); 
