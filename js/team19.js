b962e92c1d08b9e5da0ca7f4305d1df01236f99b
etapaza <etapaza@umich.edu>
2016-12-17 19:43:06 -0500

    working delete cookies

--- js/team19.js
+++ js/team19.js
@@ -25,11 +25,61 @@ chrome.tabs.query({
                 	console.log(cookies)
         			 cookies = removeDuplicates(cookies);
                     console.log(cookies);
+                    makeTable(cookies, "firstPartyCookieTable");
+                    makeTable(cookies, "thirdPartyCookieTable");
+
                 }
             }); 
         }); 
     }); 
 }); 
+
+function makeTable(cookies, id){
+
+	var numCookies = cookies.length;
+	var table = document.getElementById(id);
+	console.log(id)
+
+	var newRow = table.insertRow(0);
+	newRow.setAttribute("id", "0");
+	newRow.insertCell(0).innerHTML= "Name".bold();
+    newRow.insertCell(1).innerHTML= "Domain".bold();
+    newRow.insertCell(2).innerHTML= "Secure".bold();
+    newRow.insertCell(3).innerHTML= "Delete".bold();
+
+
+	for(i = 1; i <= numCookies; ++i){
+		var newRow = table.insertRow(i);
+		newRow.setAttribute("id", i);
+		newRow.insertCell(0).innerHTML= cookies[i-1].name;
+	    newRow.insertCell(1).innerHTML= cookies[i-1].domain;
+	    newRow.insertCell(2).innerHTML= cookies[i-1].secure;
+	    newRow.insertCell(3).innerHTML= '<button value="Delete" id="button-'+i+'"><img src="trash.png" width="20"></button>';
+	    newRow.cells[3].addEventListener('click', function(event){
+
+	    	console.log("FUCK YOU MIKE")
+	    	console.log(event.path[1].id)
+	    	var temp = event.path[1].id;
+	    	var rowNum = temp.substr(temp.length-1)
+	    	
+		    table.deleteRow(rowNum);
+
+		    for(e = 1; e <= table.rows.length; ++e) {
+		    	table.rows[e].setAttribute("id", e);
+		    	table.rows[e].cells[3].setAttribute("id", "button-"+i)
+
+		    }
+
+
+
+
+
+	    });
+
+	}
+
+}
+
 
 
 
